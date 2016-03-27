0ece230a20ccbd8602f85a7cc1d0519ff912e077
Amy Schlesener <amyschlesener@gmail.com>
2016-03-27 00:01:41 -0700

    Hook up data to display

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -80,6 +81,9 @@ public class MainActivity extends ActionBarActivity {
                     weekForecast);
 
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(forecastAdapter);
             return rootView;
         }
     }
